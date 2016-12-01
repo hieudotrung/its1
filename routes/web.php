c418c56113c8818586e6578b615aa59405122aa6
hieudo <hieudt.tkt54@gmail.com>
2016-12-01 09:55:17 +0700

    template admin

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('admin', function(){
+	return view('admin.admin_template');
+});
+
 Route::get('trangchu', function () {
     return view('layout/index_backend');
 });
